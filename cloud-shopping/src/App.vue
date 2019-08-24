815f84a1a2a017cb0ccb01ffc838fd31c0f0a715
isuru <prasadnwip@gmail.com>
2019-08-24 23:24:40 +0530

    added a color constants file. added the dashboard

--- cloud-shopping/src/App.vue
+++ cloud-shopping/src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <Dashboard/>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+  import Dashboard from "./views/Dashboard";
+
+  export default {
+    name: 'home',
+    components: {
+      Dashboard
+    }
+  }
+</script>
 
 <style lang="scss">
 #app {
---
+++ cloud-shopping/src/views/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class='Dashboard-content'>
+        <div class='page-title'>
+            <span class='chocolate'>Cloud </span>
+            <span class='goldenrod'>Shopping</span>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+    @import "../../src/constants/colors";
+    .Dashboard-content{
+        margin-top: 1vw;
+        .page-title{
+            margin-right: 40vw;
+            span{
+                font-size: 5vw;
+            }
+            .chocolate{
+                color: $chocolate;
+                margin-left: 10vw;
+            }
+            .goldenrod{
+                color: $goldenrod;
+            }
+        }
+    }
+
+</style>
