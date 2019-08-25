ac98274fd34290ce59553cda1e5bb6ac1af283af
isuru <prasadnwip@gmail.com>
2019-08-25 17:36:18 +0530

    added cart item ui elemnts

--- cloud-shopping/src/components/cart/CartContent.vue
+++ cloud-shopping/src/components/cart/CartContent.vue
@@ -1,10 +1,45 @@
 <template>
     <div class='cart-content' id="cart-content">
+
+        <!--        header-->
         <div class="header" id="header">
             <p class='title' id="cart-header">SHOPPING CART</p>
         </div>
+
+        <!--        items-->
         <div class="items" id="item-list">
             <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+        </div>
+
+        <!--        cart summery-->
+        <div class="summary" id="cart-summery">
+            <div class='total' id='total-amount'>
+                <div class='row'>
+                    <div class='col-lg-8'>total</div>
+                    <div class='col-lg-4'>Rs 500</div>
+                </div>
+            </div>
+            <div class='discounte' id='discounted-amount'>
+                <div class='row'>
+                    <div class='col-lg-8'>discount</div>
+                    <div class='col-lg-4'>100</div>
+                </div>
+            </div>
+            <div class='tax' id='tax-amount'>
+                <div class='row'>
+                    <div class='col-lg-8'>tax</div>
+                    <div class='col-lg-4'>rs 5</div>
+                </div>
+            </div>
+            <div class='final' id='final-amount'>
+                <div class='row'>
+                    <div class='col-lg-8'>final amount</div>
+                    <div class='col-lg-4'>450</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -32,6 +67,31 @@
             .title {
                 color: $white;
                 font-weight: bold;
+            }
+        }
+
+        .items {
+
+        }
+
+        .summary {
+            text-align: left;
+
+            .total {
+                margin-top: 2vh;
+            }
+
+            .discount {
+                margin-top: 2vh;
+            }
+
+            .tax {
+                margin-top: 2vh;
+            }
+
+            .final {
+                margin-top: 2vh;
+                margin-bottom: 4vh;
             }
         }
     }
--- cloud-shopping/src/components/cart/Item.vue
+++ cloud-shopping/src/components/cart/Item.vue
@@ -1,6 +1,26 @@
 <template>
-    <div>
-        <p>i am item</p>
+    <div class="item container" id="item">
+        <div class="content row" id="item-content">
+            <!--            image-->
+            <div class="image lg-col-3 md-col-3" id="item-image">
+                <img src="../../assets/products/apple.png"/>
+            </div>
+            <!--           details -->
+            <div class="details lg-col-6 md-col-6" id="item-details">
+                <div class="row" id="details-content">
+                    <div class="name col-6" id="item-name">
+                        <p>apple</p>
+                    </div>
+                    <div class="quantity col-6" id="item-quantity">
+                        <p>* 2</p>
+                    </div>
+                </div>
+            </div>
+            <!--            price-->
+            <div class="price lg-col-3 md-col-3" id="price">
+                <p>Rs 100</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,6 +30,46 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .item {
+        margin-bottom: 2vh;
+        margin-top: 2vh;
 
+        .content {
+            .image {
+                cursor: pointer;
+                display: block;
+                margin-top: 1.2vh;
+                margin-left: 1vw;
+
+                img {
+                    max-width: 6vw;
+                    max-height: 6vh;
+                    display: block;
+                    margin: auto;
+                }
+            }
+
+            .details {
+                min-width: 17vw;
+                padding-top: 2vh;
+
+                .name {
+                    max-height: 4vh;
+
+                    p {
+                        font-weight: bold;
+                    }
+                }
+
+                .quantity {
+
+                }
+            }
+
+            .price {
+                padding-top: 2vh;
+            }
+        }
+    }
 </style>
