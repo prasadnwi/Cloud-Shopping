06a7c9d641eebc4f78b70176030de00170cd1c5b
isuru <prasadnwip@gmail.com>
2019-08-27 21:50:41 +0530

    added unit test for components

--- cloud-shopping/src/views/Cart.vue
+++ cloud-shopping/src/views/Cart.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-    import {mapState, mapActions} from 'vuex';
+    import {mapState} from 'vuex';
     import CartContent from "../components/cart/CartContent";
 
     export default {
