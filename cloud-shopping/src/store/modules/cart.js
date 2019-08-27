780f38ae4c9df908785eb4e225e2c2c99ee1053a
isuru <prasadnwip@gmail.com>
2019-08-27 22:34:10 +0530

    move item records to a json file

--- cloud-shopping/src/store/modules/cart.js
+++ cloud-shopping/src/store/modules/cart.js
@@ -1,22 +1,27 @@
 const state = {
-    all: []
+    all: [] // items in the cart
 };
 
 const getters = {};
 
 const actions = {
+    /*
+    @desc add a item into the cart
+    @params object $item - added item
+    @return adding a item or incrementing the number of added items
+     */
     addItemToCart({state, commit}, item) {
-        // format item data
+
         let addedItem = {
             id: item.id,
             name: item.name,
             count: 1,
             unitPrice: item.price
         };
-// check in the cart
-        const indexofItem = state.all.findIndex(item => item.id === addedItem.id);
 
-        if (indexofItem === -1) {
+        const indexofItem = state.all.findIndex(item => item.id === addedItem.id); // check in the cart
+
+        if (indexofItem === -1) { //  added item isn't available in the cart
             commit('pusItemToCart', {addedItem})
         } else {
             commit('incrementItemQuantity', {indexofItem})
@@ -24,12 +29,17 @@ const actions = {
 
 // Todo : remove one item from item list
     },
+    /*
+    @desc remove a item from the cart
+    @params object $item - removed item
+    @return removing a item or decreasing number of removed items
+     */
     removeItemFromCart({state, commit}, item) {
 
         let updatedCart, temCart;
         const indexofItem = state.all.findIndex(existingItem => existingItem.id === item.id);
 
-        if (indexofItem !== -1 && state.all[indexofItem].count > 1) {
+        if (indexofItem !== -1 && state.all[indexofItem].count > 1) { // more than one item is available in the cart
 
             updatedCart = [...state.all];
             updatedCart[indexofItem].count--;
@@ -37,8 +47,7 @@ const actions = {
         } else if (indexofItem !== -1 && state.all[indexofItem].count === 1) {
 
             temCart = state.all.filter(existingItem => existingItem.id != item.id);
-            // if update cart is undefine, then return empty array
-            updatedCart = temCart ? temCart : [];
+            updatedCart = temCart ? temCart : [];  // if update cart is undefine, then return empty array
         }
 
         commit('updateCart', {updatedCart})
